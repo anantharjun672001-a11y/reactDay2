a2fac9cc4b9df6fb1c5f57eef03f425cc056790c
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-01 19:56:00 +0530

    parent to child values

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import Home from './Components/Home';
-import Head from './components/Head';
-import Foot from './components/Foot';
+import Home from './components/Landing Page/Home';
+import Head from './components/Landing Page/Head';
+import Foot from './components/Landing Page/Foot'; 
+import Child from './Props parent to child/Child';
+
 
 
 const App = () => {
+  let b = 20;
   return (
     <div>
-        <Home/>
-        <Head/>
-        <Foot/>
+      {/* <Header />
+      <Home />
+      <Footer /> */}
+      <h1>Parent Component B value : {b}</h1>
+      <Child b={b} />
     </div>
   );
 };
 
 export default App;
+
